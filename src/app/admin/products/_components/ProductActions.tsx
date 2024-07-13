1234e84f574f99f3c19c9de0766882ca57c6c05b
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-13 12:14:50 +0700

    Add NewProduct & EditProduct page

---
+++ src/app/admin/products/_components/ProductActions.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import { useTransition } from 'react'
+import { useRouter } from 'next/navigation'
+
+import {
+	deleteProduct,
+	toggleProductAvailability,
+} from '../../actions/products'
+import { DropdownMenuItem } from '@/components/ui/dropdown-menu'
+
+export function ActiveToggleDropdownItem({
+	id,
+	isAvailableForPurchase,
+}: {
+	id: string
+	isAvailableForPurchase: boolean
+}) {
+	const [isPending, startTransition] = useTransition()
+	const router = useRouter()
+
+	return (
+		<DropdownMenuItem
+			disabled={isPending}
+			onClick={() => {
+				startTransition(async () => {
+					await toggleProductAvailability(id, !isAvailableForPurchase)
+					router.refresh()
+				})
+			}}
+		>
+			{isAvailableForPurchase ? 'Deactivate' : 'Activate'}
+		</DropdownMenuItem>
+	)
+}
+
+export function DeleteDropdownItem({
+	id,
+	disabled,
+}: {
+	id: string
+	disabled: boolean
+}) {
+	const [isPending, startTransition] = useTransition()
+	const router = useRouter()
+
+	return (
+		<DropdownMenuItem
+			variant='destructive'
+			disabled={disabled || isPending}
+			onClick={() => {
+				startTransition(async () => {
+					await deleteProduct(id)
+					router.refresh()
+				})
+			}}
+		>
+			Delete
+		</DropdownMenuItem>
+	)
+}
--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -5,10 +5,25 @@ import { PageHeader } from '../_components/PageHeader'
 import {
 	Table,
 	TableBody,
+	TableCell,
 	TableHead,
 	TableHeader,
 	TableRow,
 } from '@/components/ui/table'
+import db from '@/db/db'
+import { CheckCircle2, MoreVertical, XCircle } from 'lucide-react'
+import { formatCurrency, formatNumber } from '@/lib/formatters'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import {
+	ActiveToggleDropdownItem,
+	DeleteDropdownItem,
+} from './_components/ProductActions'
 
 export default function AdminProducts() {
 	return (
@@ -25,6 +40,19 @@ export default function AdminProducts() {
 }
 
 async function ProductsTable() {
+	const products = await db.product.findMany({
+		select: {
+			id: true,
+			name: true,
+			priceInCents: true,
+			isAvailableForPurchase: true,
+			_count: { select: { orders: true } },
+		},
+		orderBy: { name: 'asc' },
+	})
+
+	if (products.length === 0) return <p>No products found</p>
+
 	return (
 		<Table>
 			<TableHeader>
@@ -40,6 +68,58 @@ async function ProductsTable() {
 					</TableHead>
 				</TableRow>
 			</TableHeader>
+
+			<TableBody>
+				{products.map((product) => (
+					<TableRow key={product.id}>
+						<TableCell>
+							{product.isAvailableForPurchase ? (
+								<>
+									<span className='sr-only'>Available</span>
+									<CheckCircle2 />
+								</>
+							) : (
+								<>
+									<span className='sr-only'>Unavailable</span>
+									<XCircle className='stroke-destructive' />
+								</>
+							)}
+						</TableCell>
+						<TableCell>{product.name}</TableCell>
+						<TableCell>{formatCurrency(product.priceInCents / 100)}</TableCell>
+						<TableCell>{formatNumber(product._count.orders)}</TableCell>
+						<TableCell>
+							<DropdownMenu>
+								<DropdownMenuTrigger>
+									<MoreVertical />
+									<span className='sr-only'>Actions</span>
+								</DropdownMenuTrigger>
+								<DropdownMenuContent>
+									<DropdownMenuItem asChild>
+										<a download href={`/admin/products/${product.id}/download`}>
+											Download
+										</a>
+									</DropdownMenuItem>
+									<DropdownMenuItem asChild>
+										<Link href={`/admin/products/${product.id}/edit`}>
+											Edit
+										</Link>
+									</DropdownMenuItem>
+									<ActiveToggleDropdownItem
+										id={product.id}
+										isAvailableForPurchase={product.isAvailableForPurchase}
+									/>
+									<DropdownMenuSeparator />
+									<DeleteDropdownItem
+										id={product.id}
+										disabled={product._count.orders > 0}
+									/>
+								</DropdownMenuContent>
+							</DropdownMenu>
+						</TableCell>
+					</TableRow>
+				))}
+			</TableBody>
 		</Table>
 	)
 }
