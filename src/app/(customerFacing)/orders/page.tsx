92fb830ea0091847c36569760a15bb596672e156
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-14 12:26:37 +0700

    Customer MyOrders page

---
+++ src/app/(customerFacing)/orders/page.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import { useFormState, useFormStatus } from 'react-dom'
+
+import { emailOrderHistory } from '@/actions/orders'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card'
+
+export default function MyOrders() {
+	const [data, action] = useFormState(emailOrderHistory, {})
+
+	return (
+		<form action={action} className='max-2-xl mx-auto'>
+			<Card>
+				<CardHeader>
+					<CardTitle>My Orders</CardTitle>
+					<CardDescription>
+						Enter your email and we will email you your order history and
+						download links
+					</CardDescription>
+				</CardHeader>
+
+				<CardContent>
+					<div className='space-y-2'>
+						<Label htmlFor='email'>Email</Label>
+						<Input type='email' required name='email' id='email' />
+						{data.error && <div className='text-destructive'>{data.error}</div>}
+					</div>
+				</CardContent>
+				<CardFooter>
+					{data.message ? <p>{data.message}</p> : <SubmitButton />}
+				</CardFooter>
+			</Card>
+		</form>
+	)
+}
+
+function SubmitButton() {
+	const { pending } = useFormStatus()
+
+	return (
+		<Button className='w-full' size='lg' disabled={pending} type='submit'>
+			{pending ? 'Sending...' : 'Send'}
+		</Button>
+	)
+}
